d009f163029a74c1d504cb6737a5c41a8968a530
TQT67 <trangtrinh000000@gmail.com>
2023-02-17 14:36:25 +0700

    btvn1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [number1, setNumber1] = useState();
+  const [number2, setNumber2] = useState();
+  const [sum, setSum] = useState(0);
+  const [multiply, setMultiply] = useState(0);
+
+  const handleSum = () => {
+    setSum(number1 + number2);
+  };
+  const handleMultiply = () => {
+    setMultiply(number1 * number2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="number" value={number1} onChange={(e) => setNumber1(Number(e.target.value))} />
+        <input type="number" value={number2} onChange={(e) => setNumber2(Number(e.target.value))} />
+      </div>
+      <div>
+        <button onClick={handleSum}>Sum</button>
+        <button onClick={handleMultiply}>Multiply</button>
+      </div>
+      <div>
+        <p>Sum: {sum}</p>
+        <p>Multiply: {multiply}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
